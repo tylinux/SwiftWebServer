00ca407c2ff9ee7bb723d8bf85f199949f7f11eb
tylinux <tylinux@xnure.com>
2026-07-03 23:45:32 +0800

    fix: remove artificial public typealias and use plain Foundation import

--- Sources/SwiftWebServer/SwiftWebServer.swift
+++ Sources/SwiftWebServer/SwiftWebServer.swift
@@ -1,5 +1,2 @@
-public import Foundation
+import Foundation
 @_exported public import Network
-
-// Public reference to a Foundation type so the public import is considered used.
-public typealias _SwiftWebServerFoundationUse = Data
